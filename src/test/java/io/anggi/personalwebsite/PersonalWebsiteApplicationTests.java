ed98abc6641993854dd8979d9e727f4525c3b907
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2024-06-11 14:39:03 +0300

    Initial commit

---
+++ src/test/java/io/anggi/personalwebsite/PersonalWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package io.anggi.personalwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
