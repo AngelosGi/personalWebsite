6902bfda380cde7d654fe513fdd0ac72db211d93
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2024-06-11 18:05:17 +0300

    feat: add ExperienceRepository for managing Experience entities

---
+++ src/main/java/io/anggi/personalwebsite/repository/ExperienceRepository.java
@@ -0,0 +1,9 @@
+package io.anggi.personalwebsite.repository;
+
+import io.anggi.personalwebsite.model.Experience;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExperienceRepository extends JpaRepository<Experience, Long> {
+}
