b94461f766caa3d9cfe79cc3d0fbed5c708fb843
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2024-06-11 17:58:18 +0300

    feat: add ResumeRepository for managing Resume entities

---
+++ src/main/java/io/anggi/personalwebsite/repository/ResumeRepository.java
@@ -0,0 +1,9 @@
+package io.anggi.personalwebsite.repository;
+
+import io.anggi.personalwebsite.model.Resume;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ResumeRepository extends JpaRepository<Resume, Long> {
+}
