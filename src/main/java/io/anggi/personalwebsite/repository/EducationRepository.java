7ababb6dbc95dc6d74039894316e5faadeef7722
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2024-06-11 18:04:27 +0300

    feat: add EducationRepository for managing Education entities

---
+++ src/main/java/io/anggi/personalwebsite/repository/EducationRepository.java
@@ -0,0 +1,9 @@
+package io.anggi.personalwebsite.repository;
+
+import io.anggi.personalwebsite.model.Education;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EducationRepository extends JpaRepository<Education, Long> {
+}
