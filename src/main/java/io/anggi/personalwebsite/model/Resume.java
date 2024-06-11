eed1674aebfebb7368976a50f252bb8d5bb2ce98
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2024-06-11 17:36:10 +0300

    feat: add Resume entity with relationships to Education and Experience

--- src/main/java/io/anggi/personalwebsite/model/Resume.java
+++ src/main/java/io/anggi/personalwebsite/model/Resume.java
@@ -1,4 +1,33 @@
 package io.anggi.personalwebsite.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
 public class Resume {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String jobTitle;
+    private String location;
+    private String portfolioUrl;
+    private String githubUrl;
+    private String email;
+    private String phone;
+    @Column(length = 1000)
+    private String summary;
+
+    @OneToMany(mappedBy = "resume", cascade = CascadeType.ALL)
+    private List<Education> educations;
+
+    @OneToMany(mappedBy = "resume", cascade = CascadeType.ALL)
+    private List<Experience> experiences;
+
 }
