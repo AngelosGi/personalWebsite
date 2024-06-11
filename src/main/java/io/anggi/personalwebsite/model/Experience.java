bb5beaf5b238bc1230dc8d9579dae536c11fd878
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2024-06-11 17:40:57 +0300

    feat: add Experience entity with relationship to Resume

--- src/main/java/io/anggi/personalwebsite/model/Experience.java
+++ src/main/java/io/anggi/personalwebsite/model/Experience.java
@@ -1,4 +1,22 @@
 package io.anggi.personalwebsite.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import jakarta.persistence.*;
+
+@Entity
+@Getter
+@Setter
 public class Experience {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String duration;
+    private String organization;
+
+    @ManyToOne
+    @JoinColumn(name = "resume_id")
+    private Resume resume;
 }
