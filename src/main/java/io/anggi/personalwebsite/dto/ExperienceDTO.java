8a6ea7ffd0db7cabb40985095d0c4e268c52342e
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2024-06-12 21:04:36 +0300

    Feat: Add ExperienceDTO

---
+++ src/main/java/io/anggi/personalwebsite/dto/ExperienceDTO.java
@@ -0,0 +1,13 @@
+package io.anggi.personalwebsite.dto;
+
+import lombok.Data;
+import java.util.List;
+
+@Data
+public class ExperienceDTO {
+    private Long id;
+    private String title;
+    private String duration;
+    private String organization;
+    private List<String> responsibilities;
+}
