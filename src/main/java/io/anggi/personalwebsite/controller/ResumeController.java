d05a1256b4f221de2c215cb9bab8924ba4c1eac4
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2024-06-13 01:05:44 +0300

    Feat: Add get all resume, update resume and delete resume by id endpoints

--- src/main/java/io/anggi/personalwebsite/controller/ResumeController.java
+++ src/main/java/io/anggi/personalwebsite/controller/ResumeController.java
@@ -2,9 +2,13 @@ package io.anggi.personalwebsite.controller;
 
 import io.anggi.personalwebsite.dto.ResumeDTO;
 import io.anggi.personalwebsite.mapper.ResumeMapper;
+import io.anggi.personalwebsite.model.Resume;
 import io.anggi.personalwebsite.service.ResumeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/resume")
@@ -18,5 +22,30 @@ public class ResumeController {
         return ResumeMapper.toResumeDTO(resumeService.getResumeById(id));
     }
 
+    @GetMapping
+    public List<ResumeDTO> getAllResumes() {
+        return resumeService.getAllResumes().stream()
+                .map(ResumeMapper::toResumeDTO)
+                .collect(Collectors.toList());
+    }
 
+    @PostMapping
+    public ResumeDTO createResume(@RequestBody ResumeDTO resumeDTO) {
+        Resume resume = ResumeMapper.toResumeEntity(resumeDTO);
+        Resume savedResume = resumeService.saveResume(resume);
+        return ResumeMapper.toResumeDTO(savedResume);
+    }
+
+    @PutMapping("/{id}")
+    public ResumeDTO updateResume(@PathVariable Long id, @RequestBody ResumeDTO resumeDTO) {
+        Resume resume = ResumeMapper.toResumeEntity(resumeDTO);
+        resume.setId(id); // Ensure the entity has the correct ID
+        Resume updatedResume = resumeService.saveResume(resume);
+        return ResumeMapper.toResumeDTO(updatedResume);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteResume(@PathVariable Long id) {
+        resumeService.deleteResume(id);
+    }
 }
