fcb7111f9271acfd11db06bcdcdf8828b0379fe1
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2024-06-12 22:19:47 +0300

    Feat: Add ResumeService.

---
+++ src/main/java/io/anggi/personalwebsite/service/ResumeService.java
@@ -0,0 +1,24 @@
+package io.anggi.personalwebsite.service;
+
+import io.anggi.personalwebsite.model.Resume;
+import io.anggi.personalwebsite.repository.ResumeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ResumeService {
+
+    @Autowired
+    private ResumeRepository resumeRepository;
+
+    public List<Resume> getAllResumes() {
+        return resumeRepository.findAll();
+    }
+
+    public Resume getResumeById(Long id) {
+        return resumeRepository.findById(id).orElse(null);
+    }
+
+}
