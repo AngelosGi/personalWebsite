a8435e988a0d7c177a8b1bb76341c4b1281b5365
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2024-06-12 21:03:55 +0300

    Feat: Add ResumeDTO

---
+++ src/main/java/io/anggi/personalwebsite/dto/ResumeDTO.java
@@ -0,0 +1,19 @@
+package io.anggi.personalwebsite.dto;
+
+import lombok.Data;
+import java.util.List;
+
+@Data
+public class ResumeDTO {
+    private Long id;
+    private String name;
+    private String jobTitle;
+    private String location;
+    private String portfolioUrl;
+    private String githubUrl;
+    private String email;
+    private String phone;
+    private String summary;
+    private List<EducationDTO> educations;
+    private List<ExperienceDTO> experiences;
+}
