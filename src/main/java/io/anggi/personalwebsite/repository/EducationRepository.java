8423676d486db591e18fbce0d36b15882cc018aa
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2024-06-12 18:41:35 +0300

    reset, kept only the files and structure will use dtos

--- src/main/java/io/anggi/personalwebsite/repository/EducationRepository.java
+++
@@ -1,9 +0,0 @@
-package io.anggi.personalwebsite.repository;
-
-import io.anggi.personalwebsite.model.Education;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EducationRepository extends JpaRepository<Education, Long> {
-}
--- src/main/java/io/anggi/personalwebsite/repository/ResumeRepository.java
+++
@@ -1,9 +0,0 @@
-package io.anggi.personalwebsite.repository;
-
-import io.anggi.personalwebsite.model.Resume;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ResumeRepository extends JpaRepository<Resume, Long> {
-}
