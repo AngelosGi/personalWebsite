c76b8491dc0b8b2a15c4b9d28276a4895593dda5
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2024-06-17 21:20:29 +0300

    Test: add personal data to DummyDataLoader

--- src/main/java/io/anggi/personalwebsite/DummyDataLoader.java
+++ src/main/java/io/anggi/personalwebsite/DummyDataLoader.java
@@ -27,35 +27,92 @@ public class DummyDataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Resume resume = new Resume();
-        resume.setName("John Doe");
-        resume.setJobTitle("Software Developer");
-        resume.setLocation("New York, NY");
-        resume.setPortfolioUrl("https://johndoeportfolio.com");
-        resume.setGithubUrl("https://github.com/johndoe");
-        resume.setEmail("john.doe@example.com");
-        resume.setPhone("+1 (123) 456-7890");
-        resume.setSummary("Experienced software developer with a passion for creating scalable web applications.");
+        resume.setName("Angelos Giannopoulos");
+        resume.setJobTitle("Junior Software Developer");
+        resume.setLocation("Thessaloniki, GR");
+        resume.setPortfolioUrl("http://anggi.io/");
+        resume.setLinkedinUrl("https://linkedin.com/in/anggian/");
+        resume.setGithubUrl("http://github.com/AngelosGi");
+        resume.setEmail("giannopoulos.ang@gmail.com");
+        resume.setPhone("+30 6955151477");
+        resume.setSummary("Junior software developer eager to put my knowledge into action");
 
-        Education education = new Education();
-        education.setInstitution("University of Example");
-        education.setDegree("Bachelor of Science");
-        education.setMajor("Computer Science");
-        education.setMinor("Mathematics");
-        education.setGpa(3.7);
-        education.setStartDate("2015");
-        education.setEndDate("2019");
-        education.setResume(resume);
+        Experience experience1 = new Experience();
+        experience1.setJobTitle("Technical Support Specialist");
+        experience1.setCompany("Teleperformance");
+        experience1.setStartDate("2023");
+        experience1.setEndDate("Present");
+        experience1.setDescription("Delivered technical support via phone, email, and chat for various tech products. Diagnosed and resolved software, hardware, and telecommunications issues. Collaborated with the development team to identify, document, and escalate software and platform issues by creating detailed and actionable tickets. Maintained accurate records of customer interactions using CRM software. Worked closely with team members to meet performance and quality targets. Enhanced product knowledge through ongoing training.");
+        experience1.setResponsibilities(Arrays.asList(
+                "Delivered technical support via phone, email, and chat for various tech products.",
+                "Diagnosed and resolved software, hardware, and telecommunications issues.",
+                "Collaborated with the development team to identify, document, and escalate software and platform issues by creating detailed and actionable tickets.",
+                "Maintained accurate records of customer interactions using CRM software.",
+                "Worked closely with team members to meet performance and quality targets.",
+                "Enhanced product knowledge through ongoing training."
+        ));
+        experience1.setResume(resume);
 
-        Experience experience = new Experience();
-        experience.setJobTitle("Software Engineer");
-        experience.setCompany("Tech Company XYZ");
-        experience.setStartDate("2019");
-        experience.setEndDate("Present");
-        experience.setDescription("Developed RESTful APIs using Spring Boot.\nImplemented front-end features using AngularJS.\nMaintained and optimized database queries in MySQL.");
-        experience.setResume(resume);
+        Experience experience2 = new Experience();
+        experience2.setJobTitle("Store & IT Manager");
+        experience2.setCompany("Fototypies.gr");
+        experience2.setStartDate("2012");
+        experience2.setEndDate("2023");
+        experience2.setDescription("Managed IT infrastructure, including custom software applications. Handled software updates, system integrations, and troubleshooting. Trained employees on software usage, providing ongoing technical support. Collaborated with vendors and IT teams for software customization. Oversaw store operations, including inventory control and employee management.");
+        experience2.setResponsibilities(Arrays.asList(
+                "Managed IT infrastructure, including custom software applications.",
+                "Handled software updates, system integrations, and troubleshooting.",
+                "Trained employees on software usage, providing ongoing technical support.",
+                "Collaborated with vendors and IT teams for software customization.",
+                "Oversaw store operations, including inventory control and employee management."
+        ));
+        experience2.setResume(resume);
 
-        resume.setEducationList(Arrays.asList(education));
-        resume.setExperienceList(Arrays.asList(experience));
+        Experience experience3 = new Experience();
+        experience3.setJobTitle("Store Manager, E-shop");
+        experience3.setCompany("Laventzakis' Ceramics");
+        experience3.setStartDate("2009");
+        experience3.setEndDate("2011");
+        experience3.setDescription("Managed e-shop and retail store operations. Handled customer service, order processing, and inventory control. Streamlined retail and wholesale order processes.");
+        experience3.setResponsibilities(Arrays.asList(
+                "Managed e-shop and retail store operations.",
+                "Handled customer service, order processing, and inventory control.",
+                "Streamlined retail and wholesale order processes."
+        ));
+        experience3.setResume(resume);
+
+        Experience experience4 = new Experience();
+        experience4.setJobTitle("Technical Support, Computers and networks");
+        experience4.setCompany("TripleW Internet Gaming Station");
+        experience4.setStartDate("2007");
+        experience4.setEndDate("2009");
+        experience4.setDescription("Managed IT infrastructure for 50 PCs and multiple servers. Managed software applications, games and platforms (ERP, Steam, Blizzard, CD keys). Troubleshot, coordinated software and game updates, patches, and system integrations. Trained employees on software usage and provided ongoing support and guidance.");
+        experience4.setResponsibilities(Arrays.asList(
+                "Managed IT infrastructure for 50 PCs and multiple servers.",
+                "Managed software applications, games and platforms (ERP, Steam, Blizzard, CD keys).",
+                "Troubleshot, coordinated software and game updates, patches, and system integrations.",
+                "Trained employees on software usage and provided ongoing support and guidance."
+        ));
+        experience4.setResume(resume);
+
+        Education education1 = new Education();
+        education1.setInstitution("Datalabs.edu, acta.edu - AUTH");
+        education1.setDegree("Diploma in IT, Acta - Aristotle University of Thessaloniki. (graduation)");
+        education1.setMajor("Full stack Developer - Certificate");
+        education1.setStartDate("March 2023");
+        education1.setEndDate("Present");
+        education1.setResume(resume);
+
+        Education education2 = new Education();
+        education2.setInstitution("EPAL El. Venizelou");
+        education2.setDegree("General upper secondary school certificate");
+        education2.setMajor("IT Applications Technician");
+        education2.setStartDate("2006");
+        education2.setEndDate("2009");
+        education2.setResume(resume);
+
+        resume.setEducationList(Arrays.asList(education1, education2));
+        resume.setExperienceList(Arrays.asList(experience1, experience2, experience3, experience4));
 
         resumeRepository.save(resume);
     }
