3201327463929425e365102282f896bbdfd59f30
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2024-06-27 19:30:00 +0300

    feat: add ErrorDetails class for custom error responses

---
+++ src/main/java/io/anggi/personalwebsite/exception/ErrorDetails.java
@@ -0,0 +1,14 @@
+package io.anggi.personalwebsite.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+public class ErrorDetails {
+    private Date timestamp;
+    private String message;
+    private String details;
+}
