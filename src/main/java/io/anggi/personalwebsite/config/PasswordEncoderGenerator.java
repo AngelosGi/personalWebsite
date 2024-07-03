624a8775c2530a7ea70a7e71aae558a1ad9b1453
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2024-07-04 01:02:29 +0300

    Chore: add file structure, JSON Resume Schema, commenting/uncommenting stuff.

--- src/main/java/io/anggi/personalwebsite/config/PasswordEncoderGenerator.java
+++ src/main/java/io/anggi/personalwebsite/config/PasswordEncoderGenerator.java
@@ -3,9 +3,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class PasswordEncoderGenerator {
     public static void main(String[] args) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        String rawPassword = "admin";
+        String rawPassword = "password"; //put your preferred password
         String encodedPassword = encoder.encode(rawPassword);
 
-        System.out.println(encodedPassword);
+        System.out.println(encodedPassword); //copy password and insert it to the script bellow
     }
 }
+
+// INSERT INTO app_user (username, password, role) VALUES ('user', 'encoded password from print here', 'ROLE_USER');
--- src/main/java/io/anggi/personalwebsite/service/ExperienceService.java
+++ src/main/java/io/anggi/personalwebsite/service/ExperienceService.java
@@ -14,23 +14,19 @@ public class ExperienceService {
     @Autowired
     private ExperienceRepository experienceRepository;
 
-    // Retrieves all experience records
     public List<Experience> getAllExperiences() {
         return experienceRepository.findAll();
     }
 
-    // Retrieves a specific experience record by ID, throws exception if not found
     public Experience getExperienceById(Long id) {
         return experienceRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Experience not found with id: " + id));
     }
 
-    // Saves a new or existing experience record
     public Experience saveExperience(Experience experience) {
         return experienceRepository.save(experience);
     }
 
-    // Deletes an experience record by ID, throws exception if not found
     public void deleteExperience(Long id) {
         experienceRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Experience not found with id: " + id));
