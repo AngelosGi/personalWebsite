414a7a67ad1ccf0def5958f54c6eb1407affa639
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2025-03-29 22:42:25 +0200

    refactor: delete experience entity controller, DTO, service, and repository
    Experience entity components are removed as they are better consolidated under the Resume entity for improved cohesion.

--- src/main/java/io/anggi/personalwebsite/repository/ExperienceRepository.java
+++
@@ -1,9 +0,0 @@
-package io.anggi.personalwebsite.repository;
-
-import io.anggi.personalwebsite.model.Experience;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExperienceRepository extends JpaRepository<Experience, Long> {
-}
